1fb221578aadf99a941598e7e79f024685f80b8d
Abdelkarim Amro Eltaib Molina <103129948+some-du6e@users.noreply.github.com>
2025-01-25 21:48:11 -0600

    backup stuff

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+1. [ ] Check vbucks of each account
+2. [X] Be nonchalant
+3. [ ] Do more stuff
+4. [ ] Fix stuff 
--- src/gui.py
+++ src/gui.py
@@ -2,7 +2,7 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
-import accountmanager
+import accountmanager as am
 def gui():
     window = Tk()
     # tuff
@@ -31,24 +31,43 @@ def gui():
         fill="#000000",
         font=("ComicNeue Regular", 32 * -1)
     )
-    canvas.create_text(
-        WIDTH / 2 - 150,
-        500,
-        anchor="nw",
-        text="hawjtyag",
-        fill="#000000",
-        font=("ComicNeue Regular", 32 * -1)
-    )
+    class AccountTemplate:
+        def __init__(self, nameandlevel, id):
+            self.nameandlevel = nameandlevel
+            self.id = id
+
+        def create_gui(self):
+            canvas.create_rectangle(
+                36.0,
+                60.0,
+                150.0,
+                176.0,
+                fill="#D9D9D9",
+                outline=""
+            )
+            canvas.create_text(
+                50.0,
+                70.0,
+                anchor="nw",
+                text=self.nameandlevel,
+                fill="#000000",
+                font=("ComicNeue Regular", 12 * -1)
+            )
+            button = Button(
+                window,
+                text="switch",
+                command=lambda: accountmanager.switch_account(self.id)
+            )
+            button.place(x=WIDTH / 2 - 50, y=HEIGHT - 50)
+
     # Create entries
-    for i in range(1):  # Adjust the range as needed
-        canvas.create_rectangle(
-            36.0,
-            60.0,
-            150.0,
-            176.0,
-            fill="#D9D9D9",
-            outline=""
-        )
-    print(accountmanager.get_accounts())
+
+
     window.resizable(False, False)
     window.mainloop()
+
+
+
+
+
+
